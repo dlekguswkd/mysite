b99d432b4f3e120a153f648c697f2f73f64a16fa
dlekguswkd <dlekguswkd@naver.com>
2024-08-22 18:11:44 +0900

    mysite

--- src/main/java/com/javaex/dao/UserDao.java
+++ src/main/java/com/javaex/dao/UserDao.java
@@ -1,11 +1,23 @@
 package com.javaex.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.UserVo;
 
 @Repository 
 public class UserDao {
 	
-	//@Autowired
-	//private SqlSession sqlSession;
+	@Autowired
+	private SqlSession sqlSession;
 
+	//유저 회원가입
+	public int insertPerson(UserVo userVo) {
+		
+		System.out.println("UserDao.insertPerson()");
+		int count = sqlSession.insert("user.insert", userVo);
+		
+		return count;
+		
+	}
 }
